0cf9656bd437600a8931d76b7f7b0c4c1f9dfadd
Seunghwan Chun <zshchun@gmail.com>
2023-02-22 23:57:34 +0900

    Initialize BSS section

--- main.c
+++ main.c
@@ -5,16 +5,20 @@
 #define LSR 5
 #define REG(addr) ((volatile unsigned char*)(addr))
 
+char array[128];
+
 void uart_putc(char c) {
         while ((*REG(UART_BASE+LSR) & 1<<5) == 0) { ; }
         *REG(UART_BASE+THR) = c;
 }
+
 void print(char *s) {
         while (*s) {
                 uart_putc(*s);
                 s++;
         }
 }
+
 int main() {
         print("Hello World!\n");
         return 0;
