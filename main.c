55e74298746ea319f5022885f6bbb2f1a0ddbd4c
Seunghwan Chun <zshchun@gmail.com>
2023-03-09 21:11:02 +0900

    Add test command

--- main.c
+++ main.c
@@ -5,28 +5,18 @@
 #include "timer.h"
 
 extern void rust_test();
-char arr[128];
-char test_str[] = "strcpy test\n";
-
 int lock = 0;
 
 int main(int hart_id) {
         if (hart_id) {
                 spin_lock(&lock);
                 while (1) {
-                        sleep(2);
                         spin_unlock(&lock);
                         __asm__ __volatile__("wfi");
                 }
         }
         uart_init();
         rust_test();
-        memset(arr, 'A', 20);
-        arr[20] = '\n';
-        uart_puts(arr);
-        strcpy(arr, test_str);
-        uart_puts(arr);
-        spin_lock(&lock);
         uart_puts("Hello World!\n");
         run_shell();
         return 0;
--- shell.c
+++ shell.c
@@ -3,24 +3,36 @@
 #include "string.h"
 #include "timer.h"
 
-char buf[128];
-char msg[128];
+char cmd_buf[128];
+char num[128];
+char test_str[] = "strcpy test\n";
+
 void help() {
         uart_puts("List commands\n");
         uart_puts("help : help commands\n");
+}
+void test() {
+        char arr[64];
+        memset(arr, 'A', 20);
+        arr[20] = '\n';
+        uart_puts(arr);
+        strcpy(arr, test_str);
+        uart_puts(arr);
 }
 
 void run_shell() {
         unsigned long long now;
         while (1) {
                 now = timer_read() / 1000LL;
-                lltoa(now, msg, 10);
+                lltoa(now, num, 10);
                 uart_putchar('[');
-                uart_puts(msg);
+                uart_puts(num);
                 uart_puts("] > ");
-                uart_gets(buf, sizeof(buf));
-                if (!strcmp(buf, "help")) {
+                uart_gets(cmd_buf, sizeof(cmd_buf));
+                if (!strcmp(cmd_buf, "help")) {
                         help();
+                } else if (!strcmp(cmd_buf, "test")) {
+                        test();
                 }
                 uart_putchar('\n');
         }
