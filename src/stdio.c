88944a56c3b95ef12b65dc09ade61bb496177407
Seunghwan Chun <zshchun@gmail.com>
2023-03-18 21:30:24 +0900

    Fix a bug

--- src/stdio.c
+++ src/stdio.c
@@ -1,6 +1,7 @@
 int printf(const char *format, ...) {
+        int ret;
         __builtin_va_list argv;
-        __builtin_va_start(argv, fmt);
+        __builtin_va_start(argv, format);
         // TODO parse format
         __builtin_va_end(argv);
         return ret;
