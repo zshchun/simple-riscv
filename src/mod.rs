0a919d34e7a272c21baa106a8398dbe7c5a5084e
Seunghwan Chun <zshchun@gmail.com>
2023-03-21 23:26:26 +0900

    Add printf to rust

--- src/mod.rs
+++ src/mod.rs
@@ -9,12 +9,16 @@ fn panic(_info: &PanicInfo) -> ! {
 
 extern "C" {
         fn uart_puts(input: *const u8);
+        fn printf(fmt: *const u8, ...);
 }
 #[no_mangle]
 pub extern "C" fn rust_test() {
 
         const MSG: &'static str = "Hello Rust!\n\0";
+        const PRINTF_MSG: &'static str = "Hello Rust printf %d!\n\0";
+        const PRINTF_INT: i32 = 1234;
         unsafe {
                 uart_puts(MSG.as_ptr());
+                printf(PRINTF_MSG.as_ptr(), PRINTF_INT);
         }
 }
