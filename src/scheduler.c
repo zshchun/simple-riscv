ba86141f6c842bd44381df50c5a5acb96f86c6dc
Seunghwan Chun <zshchun@gmail.com>
2023-03-25 23:57:12 +0900

    WIP save context

--- src/scheduler.c
+++ src/scheduler.c
@@ -1,2 +1,5 @@
+#include "scheduler.h"
 void sched(task_t *task) {
+//        __asm__ volatile( "la t0, (%0)\n"
+//        : : "p" (task->reg.a0) : "memory");
 }
--- src/scheduler.h
+++ src/scheduler.h
@@ -1,3 +1,14 @@
+#include <stdint.h>
+typedef struct __regs {
+        uint64_t a0;
+        uint64_t a1;
+        uint64_t a2;
+        uint64_t a3;
+        uint64_t a4;
+        uint64_t a5;
+        uint64_t a6;
+} regs_t;
 typedef struct __task {
+        regs_t reg;
 } task_t;
 void sched(task_t *task);
