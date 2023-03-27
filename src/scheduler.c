3eff4f7e76b7ef69de8b586510bd54f910fafd0c
Seunghwan Chun <zshchun@gmail.com>
2023-03-27 23:56:08 +0900

    WIP context switch

--- src/scheduler.c
+++
@@ -1,7 +0,0 @@
-#include "scheduler.h"
-void sched(task_t *task) {
-        register unsigned int sp __asm__("sp");
-        task_t *context = (sp & ~8191);
-        __asm__ __volatile__( "sd ra, 0(sp)\n"
-        : : "r" (context) : "memory");
-}
